dce52998f5ed0cd9bc0cba678789bbd73d4f1333
Angel Eduardo Lozano Niño <alozan@users.noreply.github.com>
2023-07-22 11:26:50 -0600

    Add files via upload

---
+++ prev.sh
@@ -0,0 +1,8 @@
+# Update packages list and update system
+sudo apt-get update
+sudo apt upgrade -y
+
+#Clone Repository
+git clone https://github.com/alozan/lin_inst.git
+cd lin_inst
+sudo ./install.sh
